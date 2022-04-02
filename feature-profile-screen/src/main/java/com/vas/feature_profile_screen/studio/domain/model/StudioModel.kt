9cdfde9605e264cd4c993251099df7731bd52846
Alexandr <sanchello2001@yandex.ru>
2022-04-02 18:50:24 +0300

    add model Studio

---
+++ feature-profile-screen/src/main/java/com/vas/feature_profile_screen/studio/domain/model/StudioModel.kt
@@ -0,0 +1,21 @@
+package com.vas.feature_profile_screen.studio.domain.model
+
+data class StudioModel(
+
+    val nickname : String,
+
+    val name : String,
+
+    val audioContent : String,
+
+    val videoContent : String,
+
+    val description : String,
+
+    val genre : String,
+
+    val participants : String,
+
+    val searchFor : String
+
+)
