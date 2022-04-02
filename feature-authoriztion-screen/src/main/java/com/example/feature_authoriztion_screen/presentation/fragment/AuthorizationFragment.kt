c8c83132dbaf5699bd385c22a636d0276523a107
LebedALIV2601 <lebedaliv2601@mail.ru>
2022-04-02 16:17:08 +0300

    start doing fragment layout

--- feature-authoriztion-screen/src/main/java/com/example/feature_authoriztion_screen/presentation/fragment/AuthorizationFragment.kt
+++ feature-authoriztion-screen/src/main/java/com/example/feature_authoriztion_screen/presentation/fragment/AuthorizationFragment.kt
@@ -1,10 +1,11 @@
-package com.example.feature_authoriztion_screen
+package com.example.feature_authoriztion_screen.presentation.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.feature_authoriztion_screen.R
 
 class AuthorizationFragment : Fragment() {
 
